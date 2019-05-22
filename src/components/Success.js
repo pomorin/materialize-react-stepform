2b5155f67f5c1d4dc2cb544ebc4908e3f4db1e0e
DESKTOP-DG3BGOU\Pierrot <pmorin@gmail.com>
2019-05-22 12:07:23 -0400

    change test

--- src/components/Success.js
+++ src/components/Success.js
@@ -9,7 +9,7 @@ export class Success extends Component {
         <React.Fragment>
           <AppBar title="Success" />
           <h1>Thank You For Your Submission!</h1>
-          <p>You will get email with further instructions.</p>
+          <p>You will get an email with further instructions.</p>
         </React.Fragment>
       </MuiThemeProvider>
     )
